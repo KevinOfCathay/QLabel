bca89ca89f32e7278813f0b029dcd57cda5fef8c
Kevin <kevinofcathay@gmail.com>
2023-06-12 17:56:45 +0800

    加入了 OptionCollapse 控件

---
+++ EZLabel/Custom Control/Small Tools/Option Box/OptionCollapse.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QLabel.Custom_Control.Small_Tools.Option_Box {
+	[ContentProperty("ExpanderContent")]
+	public partial class OptionCollapse : UserControl {
+		public OptionCollapse () {
+			InitializeComponent();
+		}
+		public object ExpanderContent {
+			get { return expander.Content; }
+			set { expander.Content = value; }
+		}
+
+		public object HeaderText {
+			get { return GetValue(HeaderTextProperty); }
+			set { SetValue(HeaderTextProperty, value); }
+		}
+		public static readonly DependencyProperty HeaderTextProperty =
+		    DependencyProperty.Register("HeaderText",
+			    propertyType: typeof(string), ownerType: typeof(OptionCollapse),
+			    typeMetadata: new PropertyMetadata("header"));
+
+	}
+}
